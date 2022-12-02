9148f6cf048669f382a595d1ed88ab060db65ef9
Ammar Abou Zor <ammarabouzor@gmail.com>
2022-12-02 10:53:00 +0100

    Year_15: Day 24

--- src/year_15.rs
+++ src/year_15.rs
@@ -14,6 +14,7 @@ mod day_20;
 mod day_21;
 mod day_22;
 mod day_23;
+mod day_24;
 mod day_3;
 mod day_4;
 mod day_5;
@@ -45,5 +46,6 @@ pub fn run() {
     // day_20::run();
     // day_21::run();
     // day_22::run();
-    day_23::run();
+    // day_23::run();
+    day_24::run();
 }
---
+++ src/year_15/day_24.rs
@@ -0,0 +1,131 @@
+#![allow(warnings, unused)]
+
+use rand::{rngs::ThreadRng, seq::SliceRandom};
+
+const INPUT: [usize; 29] = [
+    1, 2, 3, 5, 7, 13, 17, 19, 23, 29, 31, 37, 41, 43, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+    103, 107, 109, 113,
+];
+
+struct Solution {
+    count: usize,
+    qe: usize,
+}
+
+impl Solution {
+    fn new(count: usize, qe: usize) -> Self {
+        Self { count, qe }
+    }
+}
+
+fn get_random_solution_3(
+    mut packages: Vec<usize>,
+    target_weight: usize,
+    rng: &mut ThreadRng,
+) -> Option<Solution> {
+    packages.shuffle(rng);
+
+    let mut group_1 = vec![];
+    let mut gr_1_sum = 0;
+    let mut gr_2_sum = 0;
+    let mut gr_3_sum = 0;
+
+    while let Some(num) = packages.pop() {
+        match num {
+            weight if weight + gr_1_sum <= target_weight => {
+                gr_1_sum += weight;
+                group_1.push(weight);
+            }
+            weight if weight + gr_2_sum <= target_weight => gr_2_sum += weight,
+            weight if weight + gr_3_sum <= target_weight => gr_3_sum += weight,
+            _ => return None,
+        }
+    }
+
+    Some(Solution::new(group_1.len(), group_1.iter().product()))
+}
+
+fn get_random_solution_4(
+    mut packages: Vec<usize>,
+    target_weight: usize,
+    rng: &mut ThreadRng,
+) -> Option<Solution> {
+    packages.shuffle(rng);
+
+    let mut group_1 = vec![];
+    let mut gr_1_sum = 0;
+    let mut gr_2_sum = 0;
+    let mut gr_3_sum = 0;
+    let mut gr_4_sum = 0;
+
+    while let Some(num) = packages.pop() {
+        match num {
+            weight if weight + gr_1_sum <= target_weight => {
+                gr_1_sum += weight;
+                group_1.push(weight);
+            }
+            weight if weight + gr_2_sum <= target_weight => gr_2_sum += weight,
+            weight if weight + gr_3_sum <= target_weight => gr_3_sum += weight,
+            weight if weight + gr_4_sum <= target_weight => gr_4_sum += weight,
+            _ => return None,
+        }
+    }
+
+    Some(Solution::new(group_1.len(), group_1.iter().product()))
+}
+
+fn get_qe_ideal_conf(packages: Vec<usize>, gr_count: usize) -> usize {
+    let mut min_count = usize::MAX;
+    let mut min_qe = usize::MAX;
+
+    let mut rng = rand::thread_rng();
+    let target = packages.iter().sum::<usize>() / gr_count;
+    let func = if gr_count == 3 {
+        get_random_solution_3
+    } else {
+        get_random_solution_4
+    };
+    for _ in 0..1000_000 {
+        if let Some(solution) = func(packages.clone(), target, &mut rng) {
+            if solution.count <= min_count {
+                min_count = solution.count;
+                min_qe = min_qe.min(solution.qe);
+            }
+        }
+    }
+
+    min_qe
+}
+
+fn part_1() {
+    let qe = get_qe_ideal_conf(INPUT.to_vec(), 3);
+
+    println!("part 1: qe of ideal config is {qe}");
+}
+fn part_2() {
+    let qe = get_qe_ideal_conf(INPUT.to_vec(), 4);
+
+    println!("part 2: qe of ideal config is {qe}");
+}
+
+pub fn run() {
+    part_1();
+    part_2();
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn get_test_packages() -> Vec<usize> {
+        vec![1, 2, 3, 4, 5, 7, 8, 9, 10, 11]
+    }
+
+    #[test]
+    fn test_get_qe_ideal() {
+        let packages = get_test_packages();
+        assert_eq!(get_qe_ideal_conf(packages.clone(), 3), 99);
+        assert_eq!(get_qe_ideal_conf(packages, 4), 44);
+    }
+}
