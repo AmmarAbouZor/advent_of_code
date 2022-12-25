7af0a1b67377aa1ce5e63715be2ef3682dff6ef8
Ammar Abou Zor <ammarabouzor@gmail.com>
2022-12-25 22:42:36 +0100

    Year 22: Start

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 mod utls;
 mod year_15;
 mod year_16;
+mod year_22;
 
 fn main() {
-    run_year(16);
+    run_year(22);
 }
 
 fn run_year(year: u8) {
     match year {
         15 => year_15::run(),
         16 => year_16::run(),
+        22 => year_22::run(),
         _ => panic!("year not implemented"),
     }
 }
