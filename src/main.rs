e0dfe08c502c4b67c25f39ffc98f10d4b2580097
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-01-31 21:10:12 +0100

    Preparing to use scaffloding tool

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 mod utls;
 mod year_15;
 mod year_16;
-mod year_22;
 
 fn main() {
-    run_year(22);
+    run_year("16");
 }
 
-fn run_year(year: u8) {
+fn run_year(year: &str) {
     match year {
-        15 => year_15::run(),
-        16 => year_16::run(),
-        22 => year_22::run(),
+        "15" => year_15::run(),
+        "16" => year_16::run(),
         _ => panic!("year not implemented"),
     }
 }
