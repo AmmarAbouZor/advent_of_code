9953bd324e2eeec1d675bd3d0a1d4cfb45d1b4fa
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-02-18 22:43:51 +0100

    Year 22: Day 16 using traveling salesman algor & binary masks

--- src/year_22/day_16.rs
+++ src/year_22/day_16.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeMap, BTreeSet, BinaryHeap};
+use std::collections::BTreeMap;
 
 use itertools::Itertools;
 
@@ -19,12 +19,12 @@ impl From<&str> for Valve {
 
         let rate = parts[4]
             .strip_prefix("rate=")
-            .and_then(|txt| txt.strip_suffix(";"))
+            .and_then(|txt| txt.strip_suffix(';'))
             .and_then(|txt| txt.parse().ok())
             .unwrap();
 
         let lead_to = parts[9..]
-            .into_iter()
+            .iter()
             .map(|txt| txt.trim_end_matches(',').to_owned())
             .collect();
 
@@ -39,7 +39,7 @@ impl From<&str> for Valve {
 #[derive(Debug, PartialOrd, Ord, PartialEq, Eq, Clone)]
 struct Pair<'a>(&'a str, &'a str);
 
-fn calc_distances_floyd(valves: &Vec<Valve>) -> BTreeMap<Pair, usize> {
+fn calc_distances_floyd(valves: &[Valve]) -> BTreeMap<Pair, usize> {
     let mut distances = BTreeMap::new();
 
     valves.iter().for_each(|src| {
@@ -67,156 +67,108 @@ fn calc_distances_floyd(valves: &Vec<Valve>) -> BTreeMap<Pair, usize> {
     distances
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-struct State<'a> {
-    opened_valves: BTreeMap<&'a Valve, usize>,
-    current: &'a Valve,
+fn simulate(input: &str, minutes: usize) -> (usize, BTreeMap<u64, usize>) {
+    let valves: Vec<Valve> = input.lines().map(Valve::from).collect();
+
+    let distances = calc_distances_floyd(&valves);
+
+    let valves_with_rate_idx: BTreeMap<String, usize> = valves
+        .iter()
+        .enumerate()
+        .filter(|(_, va)| va.name == "AA" || va.rate > 0)
+        .map(|(idx, valv)| (valv.name.to_owned(), idx))
+        .collect();
+
+    let mut mask_flow: BTreeMap<u64, usize> = BTreeMap::new();
+
+    let flow = traveling_salesman(
+        &valves,
+        &mut mask_flow,
+        &valves_with_rate_idx,
+        &distances,
+        0,
+        minutes,
+        0,
+        "AA".into(),
+    );
+
+    (flow, mask_flow)
 }
 
-impl<'a> State<'a> {
-    fn new(current: &'a Valve) -> Self {
-        let opened_valves = BTreeMap::new();
+#[allow(clippy::too_many_arguments)]
+fn traveling_salesman(
+    valves: &Vec<Valve>,
+    mask_flows: &mut BTreeMap<u64, usize>,
+    valves_with_rate_idx: &BTreeMap<String, usize>,
+    distances: &BTreeMap<Pair, usize>,
+    mask: u64,
+    minutes: usize,
+    flow: usize,
+    current_valve: String,
+) -> usize {
+    let mut max = flow;
 
-        Self {
-            opened_valves,
-            current,
+    mask_flows
+        .entry(mask)
+        .and_modify(|fl| *fl = (*fl).max(flow))
+        .or_insert(flow);
+
+    for target in valves_with_rate_idx.keys() {
+        let new_minutes = minutes
+            .checked_sub(*distances.get(&Pair(&current_valve, target)).unwrap())
+            .and_then(|m| m.checked_sub(1))
+            .unwrap_or(0);
+
+        let valve_idx = *valves_with_rate_idx.get(target).unwrap();
+
+        let sub_mask = 1 << valve_idx;
+
+        if new_minutes == 0 || (mask & sub_mask) != 0 {
+            continue;
         }
+
+        let new_mask = mask | sub_mask;
+
+        let new_flow = flow + (new_minutes * valves[valve_idx].rate);
+
+        let new_max = traveling_salesman(
+            valves,
+            mask_flows,
+            valves_with_rate_idx,
+            distances,
+            new_mask,
+            new_minutes,
+            new_flow,
+            target.to_owned(),
+        );
+
+        max = max.max(new_max);
     }
-}
 
-fn calc_pressure(opened_valves: &BTreeMap<&Valve, usize>, bound: usize) -> usize {
-    let mut res = 0;
-
-    for i in 1..=bound {
-        res += opened_valves
-            .iter()
-            .filter(|(_, &time)| time < i)
-            .map(|(valve, _)| valve.rate)
-            .sum::<usize>();
-    }
-
-    res
+    max
 }
 
 fn calc_most_pressure(input: &str) -> usize {
-    let valves: Vec<Valve> = input.lines().map(Valve::from).collect();
+    let (flow, _) = simulate(input, 30);
 
-    let distances = calc_distances_floyd(&valves);
-
-    let valves_with_rate: BTreeMap<String, Valve> = valves
-        .iter()
-        .cloned()
-        .filter(|va| va.name == "AA" || va.rate > 0)
-        .map(|valva| (valva.name.clone(), valva))
-        .collect();
-
-    let mut states = BinaryHeap::new();
-
-    states.push((1, State::new(valves_with_rate.get("AA").unwrap())));
-
-    let mut result = 0;
-
-    while let Some((time, mut state)) = states.pop() {
-        if time > 30 {
-            let curr_res = calc_pressure(&state.opened_valves, 30);
-            result = result.max(curr_res);
-        } else {
-            if state.current.rate > 0 && !state.opened_valves.contains_key(state.current) {
-                state.opened_valves.insert(state.current, time);
-                states.push((time + 1, state));
-            } else {
-                for valve in valves_with_rate
-                    .values()
-                    .filter(|val| !state.opened_valves.contains_key(val))
-                {
-                    let add_time = distances
-                        .get(&Pair(state.current.name.as_str(), valve.name.as_str()))
-                        .unwrap();
-                    let mut state_cloned = state.clone();
-                    state_cloned.current = valve;
-                    states.push((time + add_time, state_cloned));
-                }
-            }
-        }
-    }
-
-    result
+    flow
 }
 
 fn calc_most_pressure_two(input: &str) -> usize {
-    let valves: Vec<Valve> = input.lines().map(Valve::from).collect();
+    let (_, mask_flow) = simulate(input, 26);
 
-    let distances = calc_distances_floyd(&valves);
+    let mut flow = 0;
 
-    let valves_with_rate: BTreeMap<String, Valve> = valves
-        .iter()
-        .cloned()
-        .filter(|va| va.name == "AA" || va.rate > 0)
-        .map(|valva| (valva.name.clone(), valva))
-        .collect();
+    mask_flow.iter().combinations(2).for_each(|comb| {
+        let (me_mask, me_score) = comb[0];
+        let (elf_mask, elf_score) = comb[1];
 
-    let mut states = BinaryHeap::new();
-
-    states.push((1, State::new(valves_with_rate.get("AA").unwrap())));
-
-    let mut results = BTreeSet::new();
-
-    while let Some((time, mut state)) = states.pop() {
-        if time > 26 {
-            results.insert(state.opened_valves);
-        } else {
-            if state.current.rate > 0 && !state.opened_valves.contains_key(state.current) {
-                state.opened_valves.insert(state.current, time);
-                states.push((time + 1, state));
-            } else {
-                for valve in valves_with_rate
-                    .values()
-                    .filter(|val| !state.opened_valves.contains_key(val))
-                {
-                    let add_time = distances
-                        .get(&Pair(state.current.name.as_str(), valve.name.as_str()))
-                        .unwrap();
-                    let mut state_cloned = state.clone();
-                    state_cloned.current = valve;
-                    states.push((time + add_time, state_cloned));
-                }
-            }
+        if (me_mask & elf_mask) == 0 {
+            flow = flow.max(me_score + elf_score);
         }
-    }
+    });
 
-    let mut results: Vec<_> = results.into_iter().collect();
-
-    results.sort_by_key(|a| calc_pressure(a, 26));
-
-    let results = results.into_iter().rev().take(500).collect_vec();
-
-    println!("start pressure calcs");
-
-    let mut pressure = 0;
-    // for (left, right) in results.iter().cartesian_product(results.iter()).unique() {
-    //     let mut clone = left.clone();
-    //     for a in left.keys() {
-    //         clone.entry(&a).and_modify(|val| {
-    //             *val = (*val).min(*right.get(a).unwrap_or(&usize::MAX));
-    //         });
-    //     }
-
-    //     pressure = pressure.max(calc_pressure(&clone, 26));
-    // }
-    for comb in results.iter().combinations(2).unique() {
-        let left = comb[0];
-        let right = comb[1];
-        let mut clone = left.clone();
-        for a in left.keys() {
-            clone.entry(&a).and_modify(|val| {
-                *val = (*val).min(*right.get(a).unwrap_or(&usize::MAX));
-            });
-        }
-
-        pressure = pressure.max(calc_pressure(&clone, 26));
-    }
-
-    pressure
+    flow
 }
 
 fn part_1() {
@@ -236,7 +188,7 @@ fn part_2() {
 }
 
 pub fn run() {
-    // part_1();
+    part_1();
     part_2();
 }
 
@@ -256,7 +208,6 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II";
 
     #[test]
-    #[ignore]
     fn test_part_1() {
         assert_eq!(calc_most_pressure(INPUT), 1651);
     }
