c7a02d1eaf9a7463dcb704c93436e8775a5fec04
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-03-01 14:39:35 +0100

    Year 22: Day 22: part 2 attempt...

--- src/year_22/day_22.rs
+++ src/year_22/day_22.rs
@@ -230,7 +230,285 @@ impl Inst {
             },
         }
     }
+
+    //      1111122222
+    //      1111122222
+    //      1111122222
+    //      1111122222
+    //      33333
+    //      33333
+    //      33333
+    //      33333
+    //      33333
+    // 5555544444
+    // 5555544444
+    // 5555544444
+    // 5555544444
+    // 5555544444
+    // 66666
+    // 66666
+    // 66666
+    // 66666
+    // 66666
+
+    fn apply_cube(&self, state: &mut State, map: &BTreeMap<Point, Tile>) {
+        match self {
+            Inst::RotateLeft => {
+                state.facing = match state.facing {
+                    Facing::Right => Facing::Up,
+                    Facing::Down => Facing::Right,
+                    Facing::Left => Facing::Down,
+                    Facing::Up => Facing::Left,
+                }
+            }
+            Inst::RotateRight => {
+                state.facing = match state.facing {
+                    Facing::Right => Facing::Down,
+                    Facing::Down => Facing::Left,
+                    Facing::Left => Facing::Up,
+                    Facing::Up => Facing::Right,
+                }
+            }
+            Inst::Move(num) => {
+                for _ in 0..*num {
+                    let mut point = state.pos;
+                    match state.facing {
+                        Facing::Right => {
+                            // Easy path
+                            if point.col % 50 != 0 {
+                                point.col += 1;
+                            } else {
+                                // Cube calculations
+                                match point.row {
+                                    // 1 & 2
+                                    (1..=50) => {
+                                        match point.col {
+                                            // 1 -> 2
+                                            100 => {
+                                                point.col += 1;
+                                            }
+                                            // 2 -> 4
+                                            150 => {
+                                                point.col = 100;
+                                                point.row = 151 - point.row;
+                                                state.facing = Facing::Left;
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    }
+                                    // 3 -> 2
+                                    (51..=100) => {
+                                        assert!((51..=100).contains(&point.col));
+                                        point.row = 100;
+                                        point.col = point.row + 50;
+                                        state.facing = Facing::Up;
+                                    }
+                                    // 4 & 5
+                                    (101..=150) => {
+                                        match point.col {
+                                            // 5 -> 4
+                                            50 => {
+                                                point.col += 1;
+                                            }
+                                            // 4 -> 2
+                                            100 => {
+                                                point.col = 150;
+                                                point.row = point.row.abs_diff(151);
+                                                state.facing = Facing::Left;
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    }
+                                    // 6 -> 4
+                                    (151..=200) => {
+                                        assert!((1..=51).contains(&point.col));
+                                        point.row = 150;
+                                        point.col = point.row - 100;
+                                        state.facing = Facing::Up;
+                                    }
+                                    _ => unreachable!(),
+                                }
+                            }
+                        }
+                        Facing::Left => {
+                            // Easy path
+                            if point.col - 1 % 50 != 0 {
+                                point.col -= 1;
+                            } else {
+                                // Cube calculations
+                                match point.row {
+                                    // 1 & 2
+                                    (1..=50) => {
+                                        match point.col {
+                                            // 1 -> 5
+                                            51 => {
+                                                point.col = 1;
+                                                point.row = 151 - point.row;
+                                                state.facing = Facing::Right;
+                                            }
+                                            // 2 -> 1
+                                            101 => {
+                                                point.col -= 1;
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    }
+                                    // 3 -> 5
+                                    (51..=100) => {
+                                        assert!((51..=100).contains(&point.col));
+                                        point.row = 101;
+                                        point.col = point.row - 50;
+                                        state.facing = Facing::Up;
+                                    }
+                                    // 4 & 5
+                                    (101..=150) => {
+                                        match point.col {
+                                            // 5 -> 1
+                                            1 => {
+                                                point.col = 51;
+                                                point.row = point.row.abs_diff(151);
+                                                state.facing = Facing::Right;
+                                            }
+                                            // 4 -> 5
+                                            51 => {
+                                                point.col -= 1;
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    }
+                                    // 6 -> 1
+                                    (151..=200) => {
+                                        assert!((1..=51).contains(&point.col));
+                                        point.row = 1;
+                                        point.col = point.row - 150;
+                                        state.facing = Facing::Down;
+                                    }
+                                    _ => unreachable!(),
+                                }
+                            }
+                        }
+                        Facing::Down => {
+                            // Easy path
+                            if point.row % 50 != 0 {
+                                point.row += 1;
+                            } else {
+                                // Cube calculations
+                                match point.row {
+                                    // 1 & 2
+                                    50 => {
+                                        match point.col {
+                                            // 1 -> 3
+                                            (51..=100) => {
+                                                point.row += 1;
+                                            }
+                                            // 2 -> 3
+                                            (101..=150) => {
+                                                point.col = 100;
+                                                point.row = point.col - 50;
+                                                state.facing = Facing::Left;
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    }
+                                    // 3 -> 4
+                                    100 => {
+                                        assert!((51..=100).contains(&point.col));
+                                        point.row += 1;
+                                    }
+                                    // 4 & 5
+                                    150 => {
+                                        match point.col {
+                                            // 5 -> 6
+                                            (1..=50) => {
+                                                point.row += 1;
+                                            }
+                                            // 4 -> 6
+                                            (51..=100) => {
+                                                point.col = 50;
+                                                point.row = point.col + 100;
+                                                state.facing = Facing::Left;
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    }
+                                    // 6 -> 2
+                                    200 => {
+                                        assert!((1..=51).contains(&point.col));
+                                        point.row = 1;
+                                        point.col = point.col + 100;
+                                        state.facing = Facing::Down;
+                                    }
+                                    _ => unreachable!(),
+                                }
+                            }
+                        }
+                        Facing::Up => {
+                            // Easy path
+                            if point.row - 1 % 50 != 0 {
+                                point.row -= 1;
+                            } else {
+                                // Cube calculations
+                                match point.row {
+                                    // 1 & 2
+                                    1 => {
+                                        match point.col {
+                                            // 1 -> 6
+                                            (51..=100) => {
+                                                point.col = 1;
+                                                point.row = point.col + 100;
+                                                state.facing = Facing::Right;
+                                            }
+                                            // 2 -> 6
+                                            (101..=150) => {
+                                                point.row = 200;
+                                                point.col = point.col - 100;
+                                                state.facing = Facing::Up;
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    }
+                                    // 3 -> 1
+                                    51 => {
+                                        assert!((51..=100).contains(&point.col));
+                                        point.row -= 1;
+                                    }
+                                    // 4 & 5
+                                    101 => {
+                                        match point.col {
+                                            // 5 -> 3
+                                            (1..=50) => {
+                                                point.col = 51;
+                                                point.row = point.col + 50;
+                                                state.facing = Facing::Right;
+                                            }
+                                            // 4 -> 3
+                                            (51..=100) => {
+                                                point.row -= 1;
+                                            }
+                                            _ => unreachable!(),
+                                        }
+                                    }
+                                    // 6 -> 5
+                                    151 => {
+                                        assert!((1..=51).contains(&point.col));
+                                        point.row -= 1;
+                                    }
+                                    _ => unreachable!(),
+                                }
+                            }
+                        }
+                    }
+
+                    match map.get(&point).expect(format!("{:?}", &point).as_str()) {
+                        Tile::Open => state.pos = point,
+                        Tile::Wall => break,
+                    }
+                }
+            }
+        }
+    }
 }
+
 fn get_final_password(input: &str) -> usize {
     let note = Note::from(input);
 
@@ -243,6 +521,18 @@ fn get_final_password(input: &str) -> usize {
     1000 * state.pos.row + 4 * state.pos.col + state.facing as usize
 }
 
+fn get_final_password_cube(input: &str) -> usize {
+    let note = Note::from(input);
+
+    let mut state = State::new(Point::new(1, 1), Facing::Right);
+
+    for ins in note.instructions.iter() {
+        ins.apply_cube(&mut state, &note.map);
+    }
+
+    1000 * state.pos.row + 4 * state.pos.col + state.facing as usize
+}
+
 fn part_1() {
     let input = read_text_from_file("22", "22");
     let answer = get_final_password(&input);
@@ -250,7 +540,12 @@ fn part_1() {
     println!("Part 1 answer is {answer}");
 }
 
-fn part_2() {}
+fn part_2() {
+    let input = read_text_from_file("22", "22");
+    let answer = get_final_password_cube(&input);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     part_1();
