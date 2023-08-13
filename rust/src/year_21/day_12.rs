598bcd23a21931c567cd57d86667f62f3ffa5dcb
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-08-13 22:45:24 +0200

    Year 21: Day 12 Part 1

---
+++ rust/src/year_21/day_12.rs
@@ -0,0 +1,174 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::utls::read_text_from_file;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+enum Node {
+    Start,
+    End,
+    Small(String),
+    Big(String),
+}
+
+impl From<&str> for Node {
+    fn from(value: &str) -> Self {
+        match value {
+            "start" => Node::Start,
+            "end" => Node::End,
+            cave => {
+                if cave.chars().all(|ch| ch.is_ascii_lowercase()) {
+                    Node::Small(cave.to_owned())
+                } else {
+                    Node::Big(cave.to_owned())
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct WayState {
+    way: Vec<Node>,
+    visited_small: HashSet<String>,
+}
+
+fn create_graph(input: &str) -> HashMap<Node, Vec<Node>> {
+    let mut graph = HashMap::new();
+    input.lines().for_each(|line| {
+        let (node_1, node_2) = line.split_once('-').unwrap();
+        let from = Node::from(node_1);
+        let to = Node::from(node_2);
+
+        graph
+            .entry(from.clone())
+            .or_insert(Vec::new())
+            .push(to.clone());
+        // We need all nodes in the graph
+        graph.entry(to).or_insert(Vec::new()).push(from);
+    });
+
+    graph
+}
+
+fn find_distinct_paths(input: &str) -> usize {
+    let graph = create_graph(input);
+
+    let mut states: Vec<_> = graph
+        .get(&Node::Start)
+        .unwrap()
+        .iter()
+        .map(|node| {
+            let way = vec![Node::Start, node.clone()];
+            let mut visited_small = HashSet::new();
+            if let Node::Small(name) = node {
+                visited_small.insert(name.clone());
+            }
+            WayState { way, visited_small }
+        })
+        .collect();
+
+    let mut ways_hash = HashSet::new();
+
+    let mut ways_count = 0;
+
+    while let Some(state) = states.pop() {
+        let last_node = state.way.last().unwrap();
+
+        if last_node == &Node::End {
+            ways_count += 1;
+            continue;
+        }
+
+        if last_node == &Node::Start {
+            continue;
+        }
+
+        for node in graph.get(last_node).unwrap().iter() {
+            let mut cloned_state = state.clone();
+
+            cloned_state.way.push(node.clone());
+            if !ways_hash.insert(cloned_state.way.clone()) {
+                continue;
+            }
+
+            if let Node::Small(name) = node {
+                if !cloned_state.visited_small.insert(name.clone()) {
+                    continue;
+                }
+            }
+
+            states.push(cloned_state);
+        }
+    }
+
+    ways_count
+}
+
+fn part_1() {
+    let input = read_text_from_file("21", "12");
+
+    let answer = find_distinct_paths(&input);
+
+    println!("Part 1 answer is {answer}");
+}
+
+fn part_2() {}
+
+pub fn run() {
+    part_1();
+    part_2();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT_1: &str = r"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+";
+
+    const INPUT_2: &str = r"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+";
+
+    const INPUT_3: &str = r"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+";
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(find_distinct_paths(INPUT_1), 10);
+        println!("one passed");
+        assert_eq!(find_distinct_paths(INPUT_2), 19);
+        assert_eq!(find_distinct_paths(INPUT_3), 226);
+    }
+}
