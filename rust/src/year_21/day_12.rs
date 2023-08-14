5b8d46f9b14a993dcdf3f5f7a3ce13f96df418e0
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-08-14 21:44:36 +0200

    Year 21: Day 12 Part 2

--- rust/src/year_21/day_12.rs
+++ rust/src/year_21/day_12.rs
@@ -30,6 +30,7 @@ impl From<&str> for Node {
 struct WayState {
     way: Vec<Node>,
     visited_small: HashSet<String>,
+    used_duplicate_small: bool,
 }
 
 fn create_graph(input: &str) -> HashMap<Node, Vec<Node>> {
@@ -63,7 +64,11 @@ fn find_distinct_paths(input: &str) -> usize {
             if let Node::Small(name) = node {
                 visited_small.insert(name.clone());
             }
-            WayState { way, visited_small }
+            WayState {
+                way,
+                visited_small,
+                used_duplicate_small: false,
+            }
         })
         .collect();
 
@@ -104,15 +109,81 @@ fn find_distinct_paths(input: &str) -> usize {
     ways_count
 }
 
+fn find_distinct_paths_with_one_duplicate(input: &str) -> usize {
+    let graph = create_graph(input);
+
+    let mut states: Vec<_> = graph
+        .get(&Node::Start)
+        .unwrap()
+        .iter()
+        .map(|node| {
+            let way = vec![Node::Start, node.clone()];
+            let mut visited_small = HashSet::new();
+            if let Node::Small(name) = node {
+                visited_small.insert(name.clone());
+            }
+            WayState {
+                way,
+                visited_small,
+                used_duplicate_small: false,
+            }
+        })
+        .collect();
+
+    let mut ways_hash = HashSet::new();
+
+    let mut ways_count = 0;
+
+    while let Some(state) = states.pop() {
+        let last_node = state.way.last().unwrap();
+
+        if last_node == &Node::End {
+            ways_count += 1;
+            continue;
+        }
+
+        if last_node == &Node::Start {
+            continue;
+        }
+
+        for node in graph.get(last_node).unwrap().iter() {
+            let mut cloned_state = state.clone();
+
+            cloned_state.way.push(node.clone());
+            if !ways_hash.insert(cloned_state.way.clone()) {
+                continue;
+            }
+
+            if let Node::Small(name) = node {
+                if !cloned_state.visited_small.insert(name.clone()) {
+                    if !cloned_state.used_duplicate_small {
+                        cloned_state.used_duplicate_small = true;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+            states.push(cloned_state);
+        }
+    }
+
+    ways_count
+}
+
 fn part_1() {
     let input = read_text_from_file("21", "12");
-
     let answer = find_distinct_paths(&input);
 
     println!("Part 1 answer is {answer}");
 }
 
-fn part_2() {}
+fn part_2() {
+    let input = read_text_from_file("21", "12");
+    let answer = find_distinct_paths_with_one_duplicate(&input);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     part_1();
@@ -167,8 +238,14 @@ start-RW
     #[test]
     fn test_part_1() {
         assert_eq!(find_distinct_paths(INPUT_1), 10);
-        println!("one passed");
         assert_eq!(find_distinct_paths(INPUT_2), 19);
         assert_eq!(find_distinct_paths(INPUT_3), 226);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(find_distinct_paths_with_one_duplicate(INPUT_1), 36);
+        assert_eq!(find_distinct_paths_with_one_duplicate(INPUT_2), 103);
+        assert_eq!(find_distinct_paths_with_one_duplicate(INPUT_3), 3509);
     }
 }
