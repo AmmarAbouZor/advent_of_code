a1268e137e92bbf2118a86bea090e89bc4154a77
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-09-01 12:58:34 +0200

    Year 21: Day 14
    Add advance solution to solve both parts efficiently

--- rust/src/year_21/day_14.rs
+++ rust/src/year_21/day_14.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use itertools::Itertools;
+
 use crate::utls::read_text_from_file;
 
 #[derive(Debug)]
@@ -34,6 +36,8 @@ impl From<&str> for Polymor {
 }
 
 impl Polymor {
+    // These two methods are the direct way to do it as the instructions but this can't handle
+    // large number of iterations. This work until 30 iterations
     fn apply_step(&mut self) {
         let mut new_template = Vec::with_capacity(self.template.len() * 2);
 
@@ -47,7 +51,6 @@ impl Polymor {
 
         self.template = new_template;
     }
-
     fn calc_diff(&self) -> usize {
         let mut count_map = HashMap::new();
 
@@ -63,25 +66,76 @@ impl Polymor {
 
         max - min
     }
+
+    /// We need to keep track on the numbers of occurrences of the each pair.
+    /// In each step we update these occurrences then we take the first characters of the pairs
+    /// Then we need to add one to the last characters from the initial template
+    fn calc_with_pair_count(&self, steps: usize) -> usize {
+        let mut pair_count = self
+            .template
+            .windows(2)
+            .map(|slice| slice.to_vec())
+            .counts();
+
+        for _ in 0..steps {
+            let mut next_count = HashMap::new();
+            for (slice, count) in pair_count.iter() {
+                let insertion = *self.insert_map.get(slice).unwrap();
+                *next_count.entry(vec![slice[0], insertion]).or_insert(0) += count;
+                *next_count.entry(vec![insertion, slice[1]]).or_insert(0) += count;
+            }
+
+            pair_count = next_count;
+        }
+
+        let mut singls_count = HashMap::new();
+
+        for (slice, count) in pair_count.into_iter() {
+            *singls_count.entry(slice[0]).or_insert(0) += count;
+        }
+
+        // Last Chars in the initial template
+        *singls_count
+            .entry(*self.template.last().unwrap())
+            .or_insert(0) += 1;
+
+        let max = singls_count.values().max().unwrap();
+        let min = singls_count.values().min().unwrap();
+
+        max - min
+    }
 }
 
-fn calc_diff(input: &str, count: usize) -> usize {
+/// This is the direct way to do it as the instructions but this can't handle
+/// large number of iterations. This work until 30 iterations
+fn calc_diff_basic_10(input: &str) -> usize {
     let mut polymor = Polymor::from(input);
 
-    for _ in 0..count {
+    for _ in 0..10 {
         polymor.apply_step();
     }
 
     polymor.calc_diff()
 }
 
+fn calc_diff_advance(input: &str, steps: usize) -> usize {
+    let polymor = Polymor::from(input);
+
+    polymor.calc_with_pair_count(steps)
+}
+
 fn part_1() {
     let input = read_text_from_file("21", "14");
-    let answer = calc_diff(&input, 10);
+    let answer = calc_diff_basic_10(&input);
 
     println!("Part 1 answer is {answer}");
 }
-fn part_2() {}
+fn part_2() {
+    let input = read_text_from_file("21", "14");
+    let answer = calc_diff_advance(&input, 40);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     part_1();
@@ -114,7 +168,12 @@ CN -> C
 
     #[test]
     fn test_part_1() {
-        assert_eq!(calc_diff(INPUT, 10), 1588);
-        assert_eq!(calc_diff(INPUT, 40), 2188189693529);
+        assert_eq!(calc_diff_basic_10(INPUT), 1588);
+    }
+
+    #[test]
+    fn test_all_parts_advanced() {
+        assert_eq!(calc_diff_advance(INPUT, 10), 1588);
+        assert_eq!(calc_diff_advance(INPUT, 40), 2188189693529);
     }
 }
