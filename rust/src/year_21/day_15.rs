5edba089a90debe7769fbdf031ce8b1846f1d19b
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-09-03 11:52:26 +0200

    Year 2021: Day 15
    Clean up part 1
    Solve part 2

--- rust/src/year_21/day_15.rs
+++ rust/src/year_21/day_15.rs
@@ -1,4 +1,4 @@
-use std::collections::BinaryHeap;
+use std::{cmp::Reverse, collections::BinaryHeap};
 
 use crate::utls::read_text_from_file;
 
@@ -22,23 +22,25 @@ impl From<&str> for Grid {
 }
 
 impl Grid {
+    // Using Dijkstra algorithm
     fn calc_lowest_risk(&self) -> i32 {
         let width = self.cells[0].len();
         let height = self.cells.len();
 
         let mut dist = vec![vec![i32::MAX; width]; height];
 
+        // Use binary heap to get the small values at first
         let mut stack = BinaryHeap::new();
 
-        // Start point
-        stack.push((0, 0, 0));
+        // Start point risk is ignored
+        stack.push((Reverse(0), 0, 0));
 
         while let Some((cost, row, col)) = stack.pop() {
             if (row, col) == (width - 1, height - 1) {
-                return -cost;
+                return cost.0;
             }
 
-            if -cost > dist[row][col] {
+            if cost.0 > dist[row][col] {
                 continue;
             }
 
@@ -49,18 +51,37 @@ impl Grid {
                 (row, col.wrapping_sub(1)),
             ] {
                 let cost_next = match self.cells.get(row_next).and_then(|r| r.get(col_next)) {
-                    Some(risk) => -cost + risk,
+                    Some(risk) => cost.0 + risk,
                     None => continue,
                 };
 
                 if cost_next < dist[row_next][col_next] {
-                    stack.push((-cost_next, row_next, col_next));
+                    stack.push((Reverse(cost_next), row_next, col_next));
                     dist[row_next][col_next] = cost_next;
                 }
             }
         }
 
         unreachable!()
+    }
+
+    fn expand(&mut self, factor: usize) {
+        let width = self.cells[0].len();
+        let height = self.cells.len();
+
+        let mut cells = vec![vec![0; width * factor]; height * factor];
+
+        for (r_idx, row) in cells.iter_mut().enumerate() {
+            for (c_idx, cell) in row.iter_mut().enumerate() {
+                let risk = self.cells[r_idx % height][c_idx % width]
+                    + (r_idx / height) as i32
+                    + (c_idx / width) as i32;
+
+                *cell = if risk > 9 { risk - 9 } else { risk };
+            }
+        }
+
+        self.cells = cells;
     }
 }
 
@@ -73,7 +94,15 @@ fn part_1() {
     println!("Part 1 answer is {answer}");
 }
 
-fn part_2() {}
+fn part_2() {
+    let input = read_text_from_file("21", "15");
+    let mut grid = Grid::from(input.as_str());
+    grid.expand(5);
+
+    let answer = grid.calc_lowest_risk();
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     part_1();
@@ -101,5 +130,13 @@ mod test {
         let grid = Grid::from(INPUT);
 
         assert_eq!(grid.calc_lowest_risk(), 40);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let mut grid = Grid::from(INPUT);
+        grid.expand(5);
+
+        assert_eq!(grid.calc_lowest_risk(), 315);
     }
 }
