bb5a05fde0abdd5b287db77ecd4dec275e7bed3f
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-09-06 21:13:58 +0200

    Year 21: Day 16: Part 1

---
+++ rust/src/year_21/day_16.rs
@@ -0,0 +1,123 @@
+use crate::utls::read_text_from_file;
+
+#[derive(Debug)]
+enum PacketType {
+    Literal,
+    Operator,
+}
+
+impl From<usize> for PacketType {
+    fn from(value: usize) -> Self {
+        match value {
+            4 => PacketType::Literal,
+            _ => PacketType::Operator,
+        }
+    }
+}
+
+fn decode_version_sum(input: &str) -> usize {
+    let input = input.trim();
+    let binary = hex_to_binary(input);
+    let bin_slice = binary.as_bytes();
+
+    // versions as vector for debugging. It can be a normal usize count too
+    let mut versions = Vec::new();
+
+    let mut idx = 0;
+
+    decode_packet(bin_slice, &mut idx, &mut versions);
+
+    versions.into_iter().sum()
+}
+
+#[inline]
+fn hex_to_binary(hex: &str) -> String {
+    hex.chars()
+        .map(|ch| ch.to_digit(16).unwrap())
+        .map(|num| format!("{:04b}", num))
+        .collect()
+}
+
+fn decode_packet(binary: &[u8], idx: &mut usize, versions: &mut Vec<usize>) {
+    let version = binary_to_num(&binary[*idx..*idx + 3]);
+    *idx += 3;
+    versions.push(version);
+
+    let packet_type: PacketType = binary_to_num(&binary[*idx..*idx + 3]).into();
+    *idx += 3;
+
+    match packet_type {
+        PacketType::Literal => {
+            let _ = parse_literal(binary, idx);
+        }
+        PacketType::Operator => parse_operator(binary, idx, versions),
+    }
+}
+
+/// This returns the sum for the literal and set the index on the next position
+fn parse_literal(binary: &[u8], idx: &mut usize) -> usize {
+    let mut literal_value = String::new();
+
+    let mut reached_end = false;
+
+    while !reached_end {
+        reached_end = binary[*idx] == b'0';
+        let binary_chunk = std::str::from_utf8(&binary[*idx + 1..*idx + 5]).unwrap();
+        literal_value.push_str(binary_chunk);
+        *idx += 5;
+    }
+
+    usize::from_str_radix(literal_value.as_str(), 2).unwrap()
+}
+
+fn parse_operator(binary: &[u8], idx: &mut usize, versions: &mut Vec<usize>) {
+    let lenght_type_id = binary[*idx];
+    *idx += 1;
+    if lenght_type_id == b'0' {
+        let bit_lenght = binary_to_num(&binary[*idx..*idx + 15]);
+        *idx += 15;
+        let start_idx = *idx;
+        while *idx < start_idx + bit_lenght {
+            decode_packet(binary, idx, versions);
+        }
+    } else {
+        let packets_count = binary_to_num(&binary[*idx..*idx + 11]);
+        *idx += 11;
+        for _ in 0..packets_count {
+            decode_packet(binary, idx, versions);
+        }
+    }
+}
+
+fn binary_to_num(slice: &[u8]) -> usize {
+    let binary_str = std::str::from_utf8(slice).unwrap();
+
+    usize::from_str_radix(binary_str, 2).unwrap()
+}
+
+fn part_1() {
+    let input = read_text_from_file("21", "16");
+    let answer = decode_version_sum(&input);
+
+    println!("Part 1 answer is {answer}");
+}
+
+fn part_2() {}
+
+pub fn run() {
+    part_1();
+    part_2();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(decode_version_sum("8A004A801A8002F478"), 16);
+        assert_eq!(decode_version_sum("620080001611562C8802118E34"), 12);
+        assert_eq!(decode_version_sum("C0015000016115A2E0802F182340"), 23);
+        assert_eq!(decode_version_sum("A0016C880162017C3686B18A3D4780"), 31);
+    }
+}
