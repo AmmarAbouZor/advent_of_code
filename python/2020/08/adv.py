c1ad12da6105eaade6052d8d6cd4a777fd9d7534
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-09-10 12:13:36 +0200

    Year 20: Day 08: Part 2

--- python/2020/08/adv.py
+++ python/2020/08/adv.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from typing import Optional, Tuple
+import copy
 
 
 def get_input() -> str:
@@ -24,6 +25,17 @@ class Instruction:
 
         raise ValueError("Invalid operation name")
 
+    def can_adjust(self) -> bool:
+        return self.oper == "jmp" or (self.oper == "nop" and self.num != 0)
+
+    def adjust(self):
+        if self.oper == "nop":
+            self.oper = "jmp"
+        elif self.oper == "jmp":
+            self.oper = "nop"
+        else:
+            raise AssertionError("operation must be nop or jmp")
+
 
 def parse_instructions(input: str) -> list[Instruction]:
     instructions = []
@@ -48,10 +60,52 @@ def get_acc_on_infinite(input: str) -> int:
     return acc
 
 
+def adjust_insts(instructions: list[Instruction], adjustments_set: set[int]):
+    for idx, inst in enumerate(instructions):
+        if idx in adjustments_set:
+            continue
+        if inst.can_adjust():
+            inst.adjust()
+            adjustments_set.add(idx)
+            return
+    raise AssertionError("Instructions must be solvable")
+
+
+def try_solve_insts(instructions: list[Instruction]) -> Optional[int]:
+    index, acc = 0, 0
+    visited_idx = set()
+    while True:
+        visited_idx.add(index)
+        idx_change, acc_change = instructions[index].apply()
+        index += idx_change
+        acc += acc_change
+        if index >= len(instructions):
+            return acc
+        if index in visited_idx:
+            return None
+
+
+def fix_loop(input: str) -> int:
+    instructions = parse_instructions(input)
+    adjustments_set = set()
+    while True:
+        insts_copy = copy.deepcopy(instructions)
+        adjust_insts(insts_copy, adjustments_set)
+        answer = try_solve_insts(insts_copy)
+        if answer is not None:
+            return answer
+
+
 def part_1():
     input = get_input()
     answer = get_acc_on_infinite(input)
     print(f"Part 1 answer is {answer}")
+
+
+def part_2():
+    input = get_input()
+    answer = fix_loop(input)
+    print(f"Part 2 answer is {answer}")
 
 
 TEST_INPUT = """nop +0
@@ -69,8 +123,11 @@ acc +6
 def test():
     acc = get_acc_on_infinite(TEST_INPUT)
     assert 5 == acc, f"Part 1: Left 5, right {acc}"
+    acc = fix_loop(TEST_INPUT)
+    assert 8 == acc, f"Part 2: Left 8, right {acc}"
     print("Tests passed")
 
 
 test()
 part_1()
+part_2()
