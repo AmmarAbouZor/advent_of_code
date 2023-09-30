3c53e24add6779f7e85212f9be53834858ec3a16
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-09-30 12:09:51 +0200

    Ruby: Year 20: Day 01: Part 02

--- ruby/2020/01/adv.rb
+++ ruby/2020/01/adv.rb
@@ -6,9 +6,8 @@ require '../../aoc_base'
 class Aoc01 < AocBase
   TARGET = 2020
 
-  def two_sum(input, target)
+  def two_sum(nums, target)
     sums = {}
-    nums = input.lines.map { |line| line.to_i }
     nums.each do |num|
       sum_val = sums[num]
       return sum_val * num unless sum_val.nil?
@@ -16,18 +15,35 @@ class Aoc01 < AocBase
       to_target = target - num
       sums[to_target] = num
     end
-  end
-
-  def part_one
-    two_sum(@input, TARGET)
-  end
-
-  def part_two
     nil
   end
 
+  def two_sum_prod(input)
+    nums = input.lines.map(&:to_i)
+    two_sum(nums, TARGET)
+  end
+
+  def three_sum_prod(input)
+    nums = input.lines.map(&:to_i)
+    nums.each_with_index do |num, idx|
+      target = TARGET - num
+      two_sum_prd = two_sum(nums[idx + 1..], target)
+      return two_sum_prd * num unless two_sum_prd.nil?
+    end
+    nil
+  end
+
+  def part_one
+    two_sum_prod(@input)
+  end
+
+  def part_two
+    three_sum_prod(@input)
+  end
+
   def do_tests
-    assert_equal two_sum(@test_input, TARGET), 514_579
+    assert_equal two_sum_prod(@test_input), 514_579
+    assert_equal three_sum_prod(@test_input), 241_861_950
     puts 'tests pass'
   end
 end
