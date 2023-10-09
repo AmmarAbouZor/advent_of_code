bb96a9901fca3b8c590590ff2222384885ca6f1c
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-10-09 21:07:22 +0200

    Ruby: Year 20: Day 08: Part 02

--- ruby/2020/08/adv.rb
+++ ruby/2020/08/adv.rb
@@ -38,13 +38,21 @@ class Aoc08 < AocBase
       when :nop then [1, 0]
       end
     end
+
+    def swap_if_possible
+      case @typ
+      when :acc then nil
+      when :jmp then Instruction.new(:nop, @num)
+      when :nop then Instruction.new(:jmp, @num)
+      end
+    end
   end
 
   def parse_input(input)
     input.lines.map(&:chomp).map { |line| Instruction.from_string(line) }
   end
 
-  def calc_acc_by_infinte(input)
+  def calc_acc_by_infinite(input)
     insts = parse_input(input)
     idx = 0
     acc = 0
@@ -58,16 +66,52 @@ class Aoc08 < AocBase
     end
   end
 
+  def try_solve(idx, acc, visited_set, insts)
+    while idx < insts.length
+      return nil if visited_set.add?(idx).nil?
+
+      idx_diff, acc_diff = insts[idx].apply
+      acc += acc_diff
+      idx += idx_diff
+    end
+    acc
+  end
+
+  def calc_acc_fixed(input)
+    insts = parse_input(input)
+    idx = 0
+    acc = 0
+    visited_idx = Set.new
+    loop do
+      swapd = insts[idx].swap_if_possible
+
+      unless swapd.nil?
+        idx_diff, acc_diff = swapd.apply
+        t_acc = acc + acc_diff
+        t_idx = idx + idx_diff
+        val = try_solve(t_idx, t_acc, visited_idx.clone, insts)
+        return val unless val.nil?
+      end
+
+      raise "Can't be duplicated in visited_set" if visited_idx.add?(idx).nil?
+
+      idx_diff, acc_diff = insts[idx].apply
+      acc += acc_diff
+      idx += idx_diff
+    end
+  end
+
   def part_one
-    calc_acc_by_infinte(@input)
+    calc_acc_by_infinite(@input)
   end
 
   def part_two
-    nil
+    calc_acc_fixed(@input)
   end
 
   def do_tests
-    assert_equal calc_acc_by_infinte(@test_input), 5
+    assert_equal calc_acc_by_infinite(@test_input), 5
+    assert_equal calc_acc_fixed(@test_input), 8
     puts 'tests pass'
   end
 end
