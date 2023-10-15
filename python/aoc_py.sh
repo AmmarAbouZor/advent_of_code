3b8f7cdd0a7b758f82844a14d335c4026ae7feea
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-10-15 22:51:10 +0200

    Python: Update scaffolding script
    - Add test file for input
    - Update code file template
    - Create bash script to open the files with neovim

---
+++ python/aoc_py.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# This is called from within the year folder
+nvim $(python3 ../aoc_py_scaff.py)
--- python/aoc_py_scaff.py
+++ python/aoc_py_scaff.py
@@ -10,12 +10,38 @@ The scripts scans the current folder to figure out which day the next day should
 """
 
 import os
+import sys
 
 INPUT_FILE = "input.txt"
 CODE_FILE = "adv.py"
-CODE_CONTENT = """def get_input_lines() -> list[str]:
+TEST_FILE = "test.txt"
+CODE_CONTENT = """def get_input() -> str:
     with open("input.txt") as f:
-        return f.read().splitlines()"""
+        return f.read()
+
+
+def get_test_input() -> str:
+    with open("test.txt") as f:
+        return f.read()
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+def run_test():
+    pass
+
+
+if __name__ == "__main__":
+    run_test()
+    part_1()
+    part_2()
+"""
 
 
 def main():
@@ -24,12 +50,20 @@ def main():
 
     input_file = os.path.join(folder_path, INPUT_FILE)
     open(input_file, "w").close()
-    print(f"file: {input_file} has been created")
+    print(f"file: {input_file} has been created", file=sys.stderr)
+
+    test_file = os.path.join(folder_path, TEST_FILE)
+    open(test_file, "w").close()
+    print(f"file: {test_file} has been created", file=sys.stderr)
 
     code_file = os.path.join(folder_path, CODE_FILE)
     with open(code_file, "w") as f:
         f.write(CODE_CONTENT)
-    print(f"file: {code_file} has been created")
+    print(f"file: {code_file} has been created", file=sys.stderr)
+
+    print(f"{test_file}")
+    print(f"{input_file}")
+    print(f"{code_file}")
 
 
 def get_next_folder_path():
