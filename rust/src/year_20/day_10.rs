8771d1b1e184493299b28384e14c48684a912d2b
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-10-19 20:36:20 +0200

    Rust: Year 20: Day 10: Part 02

--- rust/src/year_20/day_10.rs
+++ rust/src/year_20/day_10.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeMap;
+
 use crate::utls::read_text_from_file;
 
 fn get_diffs_product(input: &str) -> usize {
@@ -23,13 +25,37 @@ fn get_diffs_product(input: &str) -> usize {
     count_1 * count_3
 }
 
+fn get_arrangement_count(input: &str) -> usize {
+    let mut nums: Vec<u32> = input.lines().map(|line| line.parse().unwrap()).collect();
+    nums.sort_unstable();
+
+    let mut ways_set = BTreeMap::new();
+    ways_set.insert(0, 1);
+
+    for num in nums.iter() {
+        let ways_count = *ways_set.get(&(num - 1)).unwrap_or(&0)
+            + *ways_set.get(&(num.wrapping_sub(2))).unwrap_or(&0)
+            + *ways_set.get(&(num.wrapping_sub(3))).unwrap_or(&0);
+
+        assert!(ways_set.insert(*num, ways_count).is_none());
+    }
+
+    *ways_set.get(nums.last().unwrap()).unwrap()
+}
+
 fn part_1() {
     let input = read_text_from_file("20", "10");
     let answer = get_diffs_product(&input);
 
     println!("Part 1 answer is {answer}");
 }
-fn part_2() {}
+
+fn part_2() {
+    let input = read_text_from_file("20", "10");
+    let answer = get_arrangement_count(&input);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     part_1();
@@ -88,6 +114,12 @@ mod test {
     fn test_part_1() {
         assert_eq!(get_diffs_product(INPUT_1), 7 * 5);
         assert_eq!(get_diffs_product(INPUT_2), 22 * 10);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(get_arrangement_count(INPUT_1), 8);
+        assert_eq!(get_arrangement_count(INPUT_2), 19208);
     }
 }
 
