cb3e799ce67052a6dadc79a6734c78e5674e0a00
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-11-02 21:30:10 +0100

    Rust: Year 20: Day 12: Part 02

--- rust/src/year_20/day_12.rs
+++ rust/src/year_20/day_12.rs
@@ -65,15 +65,15 @@ impl From<&str> for Instruction {
 }
 
 #[derive(Debug)]
-struct State {
+struct StateDirection {
     east: isize,
     north: isize,
     direction: Direction,
 }
 
-impl Default for State {
+impl Default for StateDirection {
     fn default() -> Self {
-        State {
+        StateDirection {
             east: 0,
             north: 0,
             direction: Direction::East,
@@ -81,7 +81,7 @@ impl Default for State {
     }
 }
 
-impl State {
+impl StateDirection {
     fn apply_command(&mut self, instruction: Instruction) {
         match instruction.command {
             Command::North => self.north += instruction.num,
@@ -114,8 +114,113 @@ impl State {
     }
 }
 
-fn calc_distance(input: &str) -> isize {
-    let mut state = State::default();
+fn calc_distance_direction(input: &str) -> isize {
+    let mut state = StateDirection::default();
+    input.lines().map(Instruction::from).for_each(|inst| {
+        state.apply_command(inst);
+    });
+
+    state.get_manhatten_distance()
+}
+
+#[derive(Debug)]
+struct StateWaypoint {
+    pos_x: isize,
+    pos_y: isize,
+    waypoint_x: isize,
+    waypoint_y: isize,
+}
+
+impl Default for StateWaypoint {
+    fn default() -> Self {
+        StateWaypoint {
+            pos_x: 0,
+            pos_y: 0,
+            waypoint_x: 10,
+            waypoint_y: 1,
+        }
+    }
+}
+
+impl StateWaypoint {
+    fn apply_command(&mut self, instruction: Instruction) {
+        match instruction.command {
+            Command::North => self.waypoint_y += instruction.num,
+            Command::East => self.waypoint_x += instruction.num,
+            Command::South => self.waypoint_y -= instruction.num,
+            Command::West => self.waypoint_x -= instruction.num,
+            Command::Left => {
+                let count = instruction.num / 90;
+                for _ in 0..count {
+                    self.turn_left();
+                }
+            }
+            Command::Right => {
+                let count = instruction.num / 90;
+                for _ in 0..count {
+                    self.turn_right();
+                }
+            }
+            Command::Forward => {
+                self.pos_x += self.waypoint_x * instruction.num;
+                self.pos_y += self.waypoint_y * instruction.num;
+            }
+        }
+    }
+
+    fn turn_right(&mut self) {
+        match (self.waypoint_x >= 0, self.waypoint_y >= 0) {
+            // 10, 1
+            (true, true) => {
+                std::mem::swap(&mut self.waypoint_x, &mut self.waypoint_y);
+                self.waypoint_y *= -1;
+            }
+            // 1, -10
+            (true, false) => {
+                std::mem::swap(&mut self.waypoint_x, &mut self.waypoint_y);
+                self.waypoint_y *= -1;
+            }
+            // -10, -1
+            (false, false) => {
+                std::mem::swap(&mut self.waypoint_x, &mut self.waypoint_y);
+                self.waypoint_y *= -1;
+            }
+            // -1 , 10
+            (false, true) => {
+                std::mem::swap(&mut self.waypoint_x, &mut self.waypoint_y);
+                self.waypoint_y *= -1;
+            }
+        }
+    }
+
+    fn turn_left(&mut self) {
+        match (self.waypoint_x >= 0, self.waypoint_y >= 0) {
+            (true, true) => {
+                std::mem::swap(&mut self.waypoint_x, &mut self.waypoint_y);
+                self.waypoint_x *= -1;
+            }
+            (true, false) => {
+                std::mem::swap(&mut self.waypoint_x, &mut self.waypoint_y);
+                self.waypoint_x *= -1;
+            }
+            (false, false) => {
+                std::mem::swap(&mut self.waypoint_x, &mut self.waypoint_y);
+                self.waypoint_x *= -1;
+            }
+            (false, true) => {
+                std::mem::swap(&mut self.waypoint_x, &mut self.waypoint_y);
+                self.waypoint_x *= -1;
+            }
+        }
+    }
+
+    fn get_manhatten_distance(&self) -> isize {
+        self.pos_x.abs() + self.pos_y.abs()
+    }
+}
+
+fn calc_distance_waypoint(input: &str) -> isize {
+    let mut state = StateWaypoint::default();
     input.lines().map(Instruction::from).for_each(|inst| {
         state.apply_command(inst);
     });
@@ -124,12 +229,16 @@ fn calc_distance(input: &str) -> isize {
 }
 
 fn part_1(input: &str) {
-    let answer = calc_distance(input);
+    let answer = calc_distance_direction(input);
 
     println!("Part 1 answer is {answer}");
 }
 
-fn part_2(input: &str) {}
+fn part_2(input: &str) {
+    let answer = calc_distance_waypoint(input);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     let input = read_text_from_file("20", "12");
@@ -149,7 +258,8 @@ F11";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(calc_distance(INPUT), 25);
+        assert_eq!(calc_distance_direction(INPUT), 25);
+        assert_eq!(calc_distance_waypoint(INPUT), 286);
     }
 }
 
