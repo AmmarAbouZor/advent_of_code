cef78279377af5b9de06c59bec93edb46f78b51f
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-11-05 09:29:16 +0100

    Python: Year 2020: Day 12: Part 02

--- python/2020/12/adv.py
+++ python/2020/12/adv.py
@@ -1,4 +1,4 @@
-from re import error
+import math
 
 
 def get_input() -> str:
@@ -84,9 +84,57 @@ class StateDirection:
             raise Exception(f"Invalid instruction command: {inst.command}")
 
 
+class StateWaypoint:
+    def __init__(self) -> None:
+        self.pos_x = 0
+        self.pos_y = 0
+        self.way_x = 10
+        self.way_y = 1
+
+    def __repr__(self) -> str:
+        return f"Position ({self.pos_x}, {self.pos_y}), Waypoint ({self.way_x}, {self.way_y})"
+
+    def rotate(self, angle: int):
+        angle_rad = math.radians(angle)
+        sin = math.sin(angle_rad)
+        cos = math.cos(angle_rad)
+        x = self.way_x * cos - self.way_y * sin
+        y = self.way_x * sin + self.way_y * cos
+        self.way_x = round(x)
+        self.way_y = round(y)
+
+    def apply_instruction(self, inst: Instruction):
+        if inst.command == "N":
+            self.way_y += inst.num
+        elif inst.command == "S":
+            self.way_y -= inst.num
+        elif inst.command == "E":
+            self.way_x += inst.num
+        elif inst.command == "W":
+            self.way_x -= inst.num
+        elif inst.command == "L":
+            self.rotate(inst.num)
+        elif inst.command == "R":
+            self.rotate(-inst.num)
+        elif inst.command == "F":
+            self.pos_x += self.way_x * inst.num
+            self.pos_y += self.way_y * inst.num
+        else:
+            raise Exception(f"Invalid instruction command: {inst.command}")
+
+
 def get_distance_direction(input: str) -> int:
     insts = [Instruction.from_string(line) for line in input.splitlines()]
     state = StateDirection()
+    for inst in insts:
+        state.apply_instruction(inst)
+
+    return abs(state.pos_x) + abs(state.pos_y)
+
+
+def get_distance_waypoint(input: str) -> int:
+    insts = [Instruction.from_string(line) for line in input.splitlines()]
+    state = StateWaypoint()
     for inst in insts:
         state.apply_instruction(inst)
 
@@ -100,15 +148,17 @@ def part_1(input: str):
 
 
 def part_2(input: str):
-    pass
+    answer_2 = get_distance_waypoint(input)
+
+    print(f"Part 2 answer is {answer_2}")
 
 
 def run_test():
     input = get_test_input()
     answer_1 = get_distance_direction(input)
     assert answer_1 == 25, f"Expected: '{25}', Found: '{answer_1}'"
-    answer_2 = 0
-    assert answer_2 == 0, f"Expected: '{0}', Found: '{answer_2}'"
+    answer_2 = get_distance_waypoint(input)
+    assert answer_2 == 286, f"Expected: '{287}', Found: '{answer_2}'"
 
     print("Tests passed!")
 
