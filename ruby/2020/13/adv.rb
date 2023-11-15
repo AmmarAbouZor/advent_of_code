5a4d6275f5a9845a92a3e6495cf06a808e049e15
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-11-15 22:12:32 +0100

    Ruby: Year 2020: Day 13: Part 02

--- ruby/2020/13/adv.rb
+++ ruby/2020/13/adv.rb
@@ -20,12 +20,30 @@ class Aoc13 < AocBase
     target = target.to_i
 
     result = ids.split(',')
-                .map { |id| id.to_i if id.match?(/\d+/) }
-                .compact
+                .filter_map { |id| id.to_i if id.match?(/\d+/) }
                 .map { |bus_id| [min_diff(bus_id, target), bus_id] }
                 .min_by { |diff, _id| diff }
 
     result[0] * result[1]
+  end
+
+  # @param [String] input
+  def calc_earliest_match(input)
+    buses = input.lines(chomp: true)[1]
+    records = buses.split(',')
+                   .each_with_index
+                   .filter_map { |bus, idx| [idx, bus.to_i] if bus.match?(/\d+/) }
+
+    lcm = 1
+    time = 0
+    (0...(records.length - 1)).each do |i|
+      bus = records[i + 1][1]
+      idx = records[i + 1][0]
+      lcm *= records[i][1]
+      time += lcm while (time + idx) % bus != 0
+    end
+
+    time
   end
 
   def part_one
@@ -33,11 +51,12 @@ class Aoc13 < AocBase
   end
 
   def part_two
-    nil
+    calc_earliest_match(@input)
   end
 
   def do_tests
     assert_equal calc_earliest(@test_input), 295
+    assert_equal calc_earliest_match(@test_input), 1_068_781
     puts 'tests pass'
   end
 end
