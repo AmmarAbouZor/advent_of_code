bf3ba3ad6aec2bf023e2ead676fc5f0bf14102f1
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-03 10:29:44 +0100

    Rust: Year 23: Day 03: Part 2

--- rust/src/year_23/day_03.rs
+++ rust/src/year_23/day_03.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 use crate::utls::read_text_from_file;
 
@@ -38,12 +38,22 @@ impl Number {
             })
             .any(|point| symb_set.contains(&point))
     }
+
+    fn get_gear_pos(&self, gear_set: &BTreeSet<Point>) -> Option<Point> {
+        (self.start_pos.row.saturating_sub(1)..self.start_pos.row + 2)
+            .flat_map(|row| {
+                (self.start_pos.col.saturating_sub(1)..self.start_pos.col + self.length + 1)
+                    .map(move |col| Point::new(row, col))
+            })
+            .find(|point| gear_set.contains(point))
+    }
 }
 
 #[derive(Debug)]
 struct ParsedInput {
     numbers: Vec<Number>,
     symb_positions: BTreeSet<Point>,
+    gear_positions: BTreeSet<Point>,
 }
 
 impl From<&str> for ParsedInput {
@@ -53,13 +63,14 @@ impl From<&str> for ParsedInput {
 
         let mut numbers: Vec<Number> = Vec::new();
         let mut symb_positions = BTreeSet::new();
+        let mut gear_positions = BTreeSet::new();
 
         input.lines().enumerate().for_each(|(row, line)| {
             line.chars().enumerate().for_each(|(col, ch)| {
                 let mut is_digit = false;
                 match ch {
                     '.' => {}
-                    ch if ch.is_digit(10) => {
+                    ch if ch.is_ascii_digit() => {
                         is_digit = true;
                         if last_start_pos.is_none() {
                             last_start_pos = Some(Point::new(row, col));
@@ -68,6 +79,10 @@ impl From<&str> for ParsedInput {
                     }
                     ' ' => unreachable!("invalid input space"),
                     ch if ch.is_alphabetic() => unreachable!("invalid input char: {ch}"),
+                    '*' => {
+                        _ = symb_positions.insert(Point::new(row, col));
+                        _ = gear_positions.insert(Point::new(row, col));
+                    }
                     _symb => {
                         _ = symb_positions.insert(Point::new(row, col));
                     }
@@ -87,6 +102,7 @@ impl From<&str> for ParsedInput {
         ParsedInput {
             numbers,
             symb_positions,
+            gear_positions,
         }
     }
 }
@@ -102,13 +118,41 @@ fn get_valid_sum(input: &str) -> usize {
         .sum()
 }
 
+fn get_gear_sum(input: &str) -> usize {
+    let parsed = ParsedInput::from(input);
+
+    let mut sum = 0;
+    let mut adjescant_gears = BTreeMap::new();
+
+    parsed
+        .numbers
+        .iter()
+        .filter_map(|num| {
+            num.get_gear_pos(&parsed.gear_positions)
+                .map(|gear| (gear, num.value))
+        })
+        .for_each(|(gear, current_val)| {
+            if let Some(adjescant_val) = adjescant_gears.get(&gear) {
+                sum += current_val * *adjescant_val;
+            } else {
+                adjescant_gears.insert(gear, current_val);
+            }
+        });
+
+    sum
+}
+
 fn part_1(input: &str) {
     let answer_1 = get_valid_sum(input);
 
     println!("Part 1 answer is {answer_1}");
 }
 
-fn part_2(input: &str) {}
+fn part_2(input: &str) {
+    let answer_2 = get_gear_sum(input);
+
+    println!("Part 2 answer is {answer_2}");
+}
 
 pub fn run() {
     let input = read_text_from_file("23", "03");
@@ -134,6 +178,7 @@ mod test {
     #[test]
     fn test_solution() {
         assert_eq!(get_valid_sum(INPUT), 4361);
+        assert_eq!(get_gear_sum(INPUT), 467835);
     }
 }
 
