ad42bb08d0ccab256ced2f4ceef0cd7fee10a6de
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-06 14:12:07 +0100

    Rust: Year 23: Day 06: Part 2

--- rust/src/year_23/day_06.rs
+++ rust/src/year_23/day_06.rs
@@ -1,5 +1,3 @@
-use crate::utls::read_text_from_file;
-
 #[derive(Debug)]
 struct Race {
     time: usize,
@@ -23,23 +21,29 @@ fn get_win_product(races: &[Race]) -> usize {
     races.iter().map(|race| race.get_win_count()).product()
 }
 
-fn part_1(input: &[Race]) {
-    let answer = get_win_product(input);
-
-    println!("Part 1 answer is {answer}");
-}
-
-fn part_2(input: &[Race]) {}
-
-pub fn run() {
+fn part_1() {
     let input = [
         Race::new(48, 261),
         Race::new(93, 1192),
         Race::new(84, 1019),
         Race::new(66, 1063),
     ];
-    part_1(&input);
-    part_2(&input);
+    let answer = get_win_product(&input);
+
+    println!("Part 1 answer is {answer}");
+}
+
+fn part_2() {
+    let input = [Race::new(48938466, 261119210191063)];
+
+    let answer = get_win_product(&input);
+
+    println!("Part 2 answer is {answer}");
+}
+
+pub fn run() {
+    part_1();
+    part_2();
 }
 
 #[cfg(test)]
@@ -47,9 +51,11 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_() {
-        let input = [Race::new(7, 9), Race::new(15, 40), Race::new(30, 200)];
-        assert_eq!(get_win_product(&input), 288);
+    fn test_solution() {
+        let input_1 = [Race::new(7, 9), Race::new(15, 40), Race::new(30, 200)];
+        assert_eq!(get_win_product(&input_1), 288);
+        let input_2 = [Race::new(71530, 940200)];
+        assert_eq!(get_win_product(&input_2), 71503);
     }
 }
 
