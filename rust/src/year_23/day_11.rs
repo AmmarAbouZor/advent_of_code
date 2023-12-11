2748a42d6c42302f7eace7d1f2f7bab918e87afc
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-11 08:26:30 +0100

    Rust: Year 23: Day 11: Part 2

--- rust/src/year_23/day_11.rs
+++ rust/src/year_23/day_11.rs
@@ -16,7 +16,7 @@ impl Galaxy {
     }
 }
 
-fn get_expanded(input: &str) -> Vec<Galaxy> {
+fn get_expanded(input: &str, factor: usize) -> Vec<Galaxy> {
     let mut row_offset = 0;
 
     // Parse with row offset directly
@@ -31,12 +31,13 @@ fn get_expanded(input: &str) -> Vec<Galaxy> {
                 .map(|(col, _)| Galaxy::new(row + row_offset, col))
                 .collect();
             if galaxies.is_empty() {
-                row_offset += 1;
+                row_offset += factor;
             }
             galaxies
         })
         .collect();
 
+    // Add column Offset
     galaxies.sort_unstable_by_key(|gal| gal.col);
 
     let mut col_offset = 0;
@@ -45,7 +46,8 @@ fn get_expanded(input: &str) -> Vec<Galaxy> {
     let mut expanded_galaxies = Vec::with_capacity(galaxies.len());
 
     for mut galaxy in galaxies {
-        col_offset += galaxy.col.saturating_sub(last_col).saturating_sub(1);
+        let col_diff = galaxy.col.saturating_sub(last_col).saturating_sub(1);
+        col_offset += col_diff * factor;
         last_col = galaxy.col;
         galaxy.col += col_offset;
         expanded_galaxies.push(galaxy);
@@ -54,8 +56,8 @@ fn get_expanded(input: &str) -> Vec<Galaxy> {
     expanded_galaxies
 }
 
-fn calc_paths_sum(input: &str) -> usize {
-    let galaxies = get_expanded(input);
+fn calc_paths_sum(input: &str, factor: usize) -> usize {
+    let galaxies = get_expanded(input, factor);
 
     let mut sum = 0;
 
@@ -69,12 +71,16 @@ fn calc_paths_sum(input: &str) -> usize {
 }
 
 fn part_1(input: &str) {
-    let answer = calc_paths_sum(input);
+    let answer = calc_paths_sum(input, 1);
 
     println!("Part 1 answer is {answer}");
 }
 
-fn part_2(input: &str) {}
+fn part_2(input: &str) {
+    let answer = calc_paths_sum(input, 999999);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     let input = read_text_from_file("23", "11");
@@ -99,7 +105,9 @@ mod test {
 
     #[test]
     fn test_solution() {
-        assert_eq!(calc_paths_sum(INPUT), 374);
+        assert_eq!(calc_paths_sum(INPUT, 1), 374);
+        assert_eq!(calc_paths_sum(INPUT, 9), 1030);
+        assert_eq!(calc_paths_sum(INPUT, 99), 8410);
     }
 }
 
