a02a051b81d5e36745b8c706ce524a94c94ea133
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-11 08:12:37 +0100

    Rust: Year 23: Day 11: Part 1

---
+++ rust/src/year_23/day_11.rs
@@ -0,0 +1,105 @@
+use crate::utls::read_text_from_file;
+
+#[derive(Debug, Clone, Copy, Hash)]
+struct Galaxy {
+    row: usize,
+    col: usize,
+}
+
+impl Galaxy {
+    fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+
+    fn calc_distance(&self, other: &Galaxy) -> usize {
+        self.row.abs_diff(other.row) + self.col.abs_diff(other.col)
+    }
+}
+
+fn get_expanded(input: &str) -> Vec<Galaxy> {
+    let mut row_offset = 0;
+
+    // Parse with row offset directly
+    let mut galaxies: Vec<_> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(row, line)| {
+            let galaxies: Vec<_> = line
+                .chars()
+                .enumerate()
+                .filter(|(_, ch)| *ch == '#')
+                .map(|(col, _)| Galaxy::new(row + row_offset, col))
+                .collect();
+            if galaxies.is_empty() {
+                row_offset += 1;
+            }
+            galaxies
+        })
+        .collect();
+
+    galaxies.sort_unstable_by_key(|gal| gal.col);
+
+    let mut col_offset = 0;
+    let mut last_col = 0_usize;
+
+    let mut expanded_galaxies = Vec::with_capacity(galaxies.len());
+
+    for mut galaxy in galaxies {
+        col_offset += galaxy.col.saturating_sub(last_col).saturating_sub(1);
+        last_col = galaxy.col;
+        galaxy.col += col_offset;
+        expanded_galaxies.push(galaxy);
+    }
+
+    expanded_galaxies
+}
+
+fn calc_paths_sum(input: &str) -> usize {
+    let galaxies = get_expanded(input);
+
+    let mut sum = 0;
+
+    for (idx, gal) in galaxies.iter().enumerate() {
+        for other_gal in galaxies.iter().skip(idx + 1) {
+            sum += gal.calc_distance(other_gal);
+        }
+    }
+
+    sum
+}
+
+fn part_1(input: &str) {
+    let answer = calc_paths_sum(input);
+
+    println!("Part 1 answer is {answer}");
+}
+
+fn part_2(input: &str) {}
+
+pub fn run() {
+    let input = read_text_from_file("23", "11");
+    part_1(&input);
+    part_2(&input);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
+
+    #[test]
+    fn test_solution() {
+        assert_eq!(calc_paths_sum(INPUT), 374);
+    }
+}
+
