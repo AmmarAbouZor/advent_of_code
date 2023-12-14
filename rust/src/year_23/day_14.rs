6fa3f6788db5dd4fefec78895cc7129840a8eadf
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-14 08:26:50 +0100

    Rust: Year 23: Day 14: Part 2.

--- rust/src/year_23/day_14.rs
+++ rust/src/year_23/day_14.rs
@@ -1,8 +1,25 @@
 use crate::utls::read_text_from_file;
 
-fn tilt_north(input: &str) -> Vec<Vec<u8>> {
-    let mut cells: Vec<_> = input.lines().map(|line| line.as_bytes().to_vec()).collect();
+#[derive(Debug)]
+enum Dir {
+    North,
+    West,
+    South,
+    East,
+}
 
+impl Dir {
+    fn next(self) -> Dir {
+        match self {
+            Dir::North => Dir::West,
+            Dir::West => Dir::South,
+            Dir::South => Dir::East,
+            Dir::East => Dir::North,
+        }
+    }
+}
+
+fn tilt_north(cells: &mut Vec<Vec<u8>>) {
     for row in 1..cells.len() {
         for col in 0..cells[0].len() {
             if cells[row][col] != b'O' {
@@ -10,19 +27,99 @@ fn tilt_north(input: &str) -> Vec<Vec<u8>> {
             }
 
             let mut curr_row = row;
-            while let Some(r) = curr_row.checked_sub(1) {
-                if cells[r][col] == b'.' {
-                    cells[r][col] = b'O';
+            while let Some(prev_row) = curr_row.checked_sub(1) {
+                if cells[prev_row][col] == b'.' {
+                    cells[prev_row][col] = b'O';
                     cells[curr_row][col] = b'.';
-                    curr_row = r;
+                    curr_row = prev_row;
                 } else {
                     break;
                 }
             }
         }
     }
+}
 
-    cells
+fn tilt_south(cells: &mut Vec<Vec<u8>>) {
+    for row in (0..cells.len() - 1).rev() {
+        for col in 0..cells[0].len() {
+            if cells[row][col] != b'O' {
+                continue;
+            }
+
+            let mut curr_row = row;
+            while curr_row < cells.len() - 1 {
+                let next_row = curr_row + 1;
+                if cells[next_row][col] == b'.' {
+                    cells[next_row][col] = b'O';
+                    cells[curr_row][col] = b'.';
+                    curr_row = next_row;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_west(cells: &mut Vec<Vec<u8>>) {
+    for row in 0..cells.len() {
+        for col in 1..cells[0].len() {
+            if cells[row][col] != b'O' {
+                continue;
+            }
+
+            let mut curr_col = col;
+            while let Some(prev_col) = curr_col.checked_sub(1) {
+                if cells[row][prev_col] == b'.' {
+                    cells[row][prev_col] = b'O';
+                    cells[row][curr_col] = b'.';
+                    curr_col = prev_col;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_east(cells: &mut Vec<Vec<u8>>) {
+    for row in 0..cells.len() {
+        for col in (0..cells[0].len() - 1).rev() {
+            if cells[row][col] != b'O' {
+                continue;
+            }
+
+            let mut curr_col = col;
+            while curr_col < cells[0].len() - 1 {
+                let next_col = curr_col + 1;
+                if cells[row][next_col] == b'.' {
+                    cells[row][next_col] = b'O';
+                    cells[row][curr_col] = b'.';
+                    curr_col = next_col;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn tilt_cycles(cells: &mut Vec<Vec<u8>>) {
+    let mut dir = Dir::North;
+    // for _ in 0..1000000000 {
+    // it settle down after 1000 in my input
+    for _ in 0..1000 {
+        for _ in 0..4 {
+            match dir {
+                Dir::North => tilt_north(cells),
+                Dir::West => tilt_west(cells),
+                Dir::South => tilt_south(cells),
+                Dir::East => tilt_east(cells),
+            }
+            dir = dir.next();
+        }
+    }
 }
 
 #[allow(unused)]
@@ -34,11 +131,14 @@ fn print_cells(cells: &[Vec<u8>]) {
     });
 }
 
-fn calc_north_load(input: &str) -> usize {
-    let titled = tilt_north(input);
-    // print_cells(&titled);
+fn calc_load<F>(input: &str, tilt: F) -> usize
+where
+    F: Fn(&mut Vec<Vec<u8>>),
+{
+    let mut cells: Vec<_> = input.lines().map(|line| line.as_bytes().to_vec()).collect();
+    tilt(&mut cells);
 
-    titled
+    cells
         .iter()
         .rev()
         .enumerate()
@@ -47,12 +147,16 @@ fn calc_north_load(input: &str) -> usize {
 }
 
 fn part_1(input: &str) {
-    let answer = calc_north_load(input);
+    let answer = calc_load(input, tilt_north);
 
     println!("Part 1 answer is {answer}");
 }
 
-fn part_2(input: &str) {}
+fn part_2(input: &str) {
+    let answer = calc_load(input, tilt_cycles);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     let input = read_text_from_file("23", "14");
@@ -77,7 +181,8 @@ O.#..O.#.#
 
     #[test]
     fn test_solution() {
-        assert_eq!(calc_north_load(INPUT), 136);
+        assert_eq!(calc_load(INPUT, tilt_north), 136);
+        assert_eq!(calc_load(INPUT, tilt_cycles), 64);
     }
 }
 
