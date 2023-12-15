a36ce8fdaa1265aa8b7225956457bbb076e0982b
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-15 08:15:58 +0100

    Rust: Year 23: Day 15: Part 2.

--- rust/src/year_23/day_15.rs
+++ rust/src/year_23/day_15.rs
@@ -16,13 +16,53 @@ fn calc_sum(input: &str) -> usize {
     input.split(',').map(calc_hash).sum()
 }
 
+fn calc_focal_sum(input: &str) -> usize {
+    let mut map: Vec<Vec<(&str, usize)>> = vec![Vec::new(); 256];
+    for chunk in input.split(',') {
+        let chunk = chunk.trim_end_matches('\n');
+        // Remove case
+        if let Some(rm_lbl) = chunk.strip_suffix('-') {
+            let hash = calc_hash(rm_lbl);
+            let que = map.get_mut(hash).unwrap();
+            que.retain(|&(lbl, _)| lbl != rm_lbl)
+        }
+        // Add case
+        else {
+            let chunk = chunk.trim_matches('\n');
+            let (lbl, num) = chunk.split_once('=').unwrap();
+            let num = num.parse().unwrap();
+            let hash = calc_hash(lbl);
+            let que = map.get_mut(hash).unwrap();
+            if let Some(entry) = que.iter_mut().find(|(l, _)| *l == lbl) {
+                entry.1 = num;
+            } else {
+                que.push((lbl, num))
+            }
+        }
+    }
+
+    map.into_iter()
+        .enumerate()
+        .map(|(box_idx, lbls)| {
+            lbls.iter()
+                .enumerate()
+                .map(|(lbl_idx, (_lbl, focal))| (box_idx + 1) * (lbl_idx + 1) * *focal)
+                .sum::<usize>()
+        })
+        .sum()
+}
+
 fn part_1(input: &str) {
     let answer = calc_sum(input);
 
     println!("Part 1 answer is {answer}");
 }
 
-fn part_2(input: &str) {}
+fn part_2(input: &str) {
+    let answer = calc_focal_sum(input);
+
+    println!("Part 2 asnwer is {answer}");
+}
 
 pub fn run() {
     let input = read_text_from_file("23", "15");
@@ -39,6 +79,7 @@ mod test {
     #[test]
     fn test_solution() {
         assert_eq!(calc_sum(INPUT), 1320);
+        assert_eq!(calc_focal_sum(INPUT), 145);
     }
 }
 
