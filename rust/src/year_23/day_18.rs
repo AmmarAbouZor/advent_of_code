23a404e9cf3b907b12e24c9f8673314fff028160
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-18 13:35:29 +0100

    Rust: Year 23: Day 18: Part 2

--- rust/src/year_23/day_18.rs
+++ rust/src/year_23/day_18.rs
@@ -24,7 +24,6 @@ enum Dir {
 struct Inst {
     dir: Dir,
     count: isize,
-    color: u64,
 }
 
 impl From<&str> for Inst {
@@ -40,20 +39,35 @@ impl From<&str> for Inst {
 
         let count = parts.next().and_then(|num| num.parse().ok()).unwrap();
 
-        let mut color = parts.next().unwrap();
-
-        color = color.trim_start_matches("(#");
-        color = color.trim_end_matches(')');
-        let color = u64::from_str_radix(color, 16).unwrap();
-
-        Self { dir, count, color }
+        Self { dir, count }
     }
 }
 
-fn get_vertices(input: &str) -> Vec<Point> {
+impl Inst {
+    fn from_hex(line: &str) -> Self {
+        let (_, mut hex) = line.split_once("(#").unwrap();
+        hex = hex.trim_end_matches(')');
+        let dir = match isize::from_str_radix(&hex[5..], 16).unwrap() {
+            0 => Dir::East,
+            1 => Dir::South,
+            2 => Dir::West,
+            3 => Dir::North,
+            invalid => unreachable!("{invalid}"),
+        };
+
+        let count = isize::from_str_radix(&hex[..5], 16).unwrap();
+
+        Self { dir, count }
+    }
+}
+
+fn get_vertices<F>(input: &str, parse_func: F) -> Vec<Point>
+where
+    F: Fn(&str) -> Inst,
+{
     input
         .lines()
-        .map(Inst::from)
+        .map(parse_func)
         .fold(vec![Point::new(0, 0)], |mut vertices, inst| {
             let next_dir = match inst.dir {
                 Dir::North => (0, 1),
@@ -62,7 +76,7 @@ fn get_vertices(input: &str) -> Vec<Point> {
                 Dir::West => (-1, 0),
             };
 
-            let mut vertex = vertices.last().unwrap().clone();
+            let mut vertex = *vertices.last().unwrap();
             vertex.x += next_dir.0 * inst.count;
             vertex.y += next_dir.1 * inst.count;
 
@@ -84,21 +98,30 @@ fn calc_filled(vertices: &[Point]) -> usize {
         perimeter += perimeter_diff;
     }
 
-    ((area.abs() as usize / 2) - (perimeter / 2) + 1) + perimeter
+    ((area.unsigned_abs() / 2) - (perimeter / 2) + 1) + perimeter
 }
 
 fn calc_cubes_count(input: &str) -> usize {
-    let vertcies = get_vertices(input);
-    calc_filled(&vertcies)
+    let vertices = get_vertices(input, |line| Inst::from(line));
+    calc_filled(&vertices)
+}
+
+fn calc_from_hex(input: &str) -> usize {
+    let vertices = get_vertices(input, Inst::from_hex);
+    calc_filled(&vertices)
 }
 
 fn part_1(input: &str) {
     let answer = calc_cubes_count(input);
 
-    println!("Part 1 asnwer is {answer}");
+    println!("Part 1 answer is {answer}");
 }
 
-fn part_2(input: &str) {}
+fn part_2(input: &str) {
+    let answer = calc_from_hex(input);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     let input = read_text_from_file("23", "18");
@@ -128,6 +151,7 @@ U 2 (#7a21e3)";
     #[test]
     fn test_solution() {
         assert_eq!(calc_cubes_count(INPUT), 62);
+        assert_eq!(calc_from_hex(INPUT), 952408144115);
     }
 }
 
