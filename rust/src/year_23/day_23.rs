76234b250c547a19da318eb031c36e0cf1bde582
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-23 19:29:10 +0100

    Rust: Year 23: Day 23: Part 2: Solved with graphs

--- rust/src/year_23/day_23.rs
+++ rust/src/year_23/day_23.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
@@ -154,67 +154,96 @@ impl Maze {
         max_steps
     }
 
-    fn get_next_moves_no_slope(&self, state: &State) -> Vec<Point> {
-        [
-            Point::new(state.curr.row - 1, state.curr.col),
-            Point::new(state.curr.row + 1, state.curr.col),
-            Point::new(state.curr.row, state.curr.col - 1),
-            Point::new(state.curr.row, state.curr.col + 1),
-        ]
-        .into_iter()
-        .filter(|p| {
-            *p != state.prev && matches!(self.get_content(p), b'.' | b'v' | b'^' | b'<' | b'>')
-        })
-        .collect()
+    fn get_next_moves_no_slope(&self, point: &Point) -> Vec<Point> {
+        let mut points = Vec::new();
+        if point.row > 0 {
+            points.push(Point::new(point.row - 1, point.col));
+        }
+        if point.row < self.cells.len() - 1 {
+            points.push(Point::new(point.row + 1, point.col));
+        }
+
+        if point.col > 0 {
+            points.push(Point::new(point.row, point.col - 1));
+        }
+
+        if point.col < self.cells[0].len() - 1 {
+            points.push(Point::new(point.row, point.col + 1));
+        }
+
+        points
+            .into_iter()
+            .filter(|p| self.get_content(p) != b'#')
+            .collect()
     }
 
-    fn find_max_steps_no_slope(&self) -> usize {
-        // real input and the test has the first entrance
-        let initial_state = State::new(Point::new(1, 1), Point::new(0, 1), Vec::new());
-        let mut queue = VecDeque::new();
-        // We've already made the first step
-        queue.push_back((1, initial_state.clone()));
+    fn get_longest_dist_recr(
+        start: &Point,
+        end: &Point,
+        map: &HashMap<Point, HashMap<Point, usize>>,
+        visited: &HashSet<Point>,
+    ) -> usize {
+        if start == end {
+            return 0;
+        }
 
-        let mut states_map: HashMap<State, usize> = HashMap::new();
+        let mut visited_clone = visited.clone();
+        visited_clone.insert(*start);
 
-        let mut max_steps = 0;
-
-        while let Some((mut steps, state)) = queue.pop_front() {
-            if self.reached_target(&state.curr) {
-                max_steps = max_steps.max(steps);
-                continue;
-            }
-
-            if let Some(saved_steps) = states_map.get_mut(&state) {
-                if steps > *saved_steps {
-                    *saved_steps = steps;
-                } else {
-                    continue;
-                }
-            }
-
-            steps += 1;
-            for p in self.get_next_moves_no_slope(&state) {
-                let mut clone = state.clone();
-                let next_point = match self.get_content(&p) {
-                    b'^' | b'v' | b'<' | b'>' | b'.' => {
-                        if clone.visited_slopes.contains(&p) {
-                            continue;
-                        }
-                        clone.visited_slopes.push(p);
-
-                        p
-                    }
-                    invalid => unreachable!("Invalid byte: '{invalid}'"),
-                };
-
-                clone.prev = clone.curr;
-                clone.curr = next_point;
-                queue.push_back((steps, clone));
+        let mut distance = 0_usize;
+        let start_node_map = map.get(start).unwrap();
+        for neighbor in start_node_map.keys() {
+            if !visited_clone.contains(neighbor) {
+                let next_dist = Self::get_longest_dist_recr(neighbor, end, map, &visited_clone);
+                distance = distance.max(next_dist + *start_node_map.get(neighbor).unwrap());
             }
         }
 
-        max_steps
+        distance
+    }
+
+    fn find_max_steps_graph(&self) -> usize {
+        let start_pos = Point::new(0, 1);
+        let end_pos = Point::new(self.cells.len() - 1, self.cells[0].len() - 2);
+
+        // Define the node points so we can fill them in the graph below
+        let mut node_points = vec![start_pos, end_pos];
+        let nodes = (0..self.cells.len())
+            .flat_map(|row| (0..self.cells[0].len()).map(move |col| Point::new(row, col)))
+            .filter(|p| self.get_content(p) != b'#' && self.get_next_moves_no_slope(p).len() > 2);
+
+        node_points.extend(nodes);
+
+        // Fill the Graph with distances
+        let mut graph: HashMap<Point, HashMap<Point, usize>> =
+            node_points.iter().map(|p| (*p, HashMap::new())).collect();
+
+        for node in &node_points {
+            let mut queue: Vec<(Point, usize)> = vec![(*node, 0)];
+            let mut visited: HashSet<Point> = HashSet::new();
+
+            while let Some((curr_point, curr_dist)) = queue.pop() {
+                if node_points.contains(&curr_point) && curr_dist != 0 {
+                    graph
+                        .entry(*node)
+                        .or_default()
+                        .entry(curr_point)
+                        .or_insert(curr_dist);
+                    continue;
+                }
+
+                for next_point in self.get_next_moves_no_slope(&curr_point) {
+                    if visited.insert(next_point) {
+                        queue.push((next_point, curr_dist + 1));
+                    }
+                }
+
+                visited.insert(curr_point);
+            }
+        }
+
+        // Use the graph to find the answer recursively
+        Self::get_longest_dist_recr(&start_pos, &end_pos, &graph, &HashSet::new())
     }
 }
 
@@ -227,7 +256,7 @@ fn find_max_steps(input: &'static str) -> usize {
 fn find_max_no_slope(input: &'static str) -> usize {
     let maze = Maze::from(input);
 
-    maze.find_max_steps_no_slope()
+    maze.find_max_steps_graph()
 }
 
 fn part_1(input: &'static str) {
@@ -243,9 +272,7 @@ fn part_2(input: &'static str) {
 }
 
 pub fn run() {
-    //TODO: uncomment the first input and remove the later when the solution is solved
-    // let input = crate::utls::read_text_from_file("23", "23").leak();
-    let input = crate::include_input!("23", "23");
+    let input = crate::utls::read_text_from_file("23", "23").leak();
     part_1(input);
     part_2(input);
 }
@@ -284,4 +311,3 @@ mod test {
         assert_eq!(find_max_no_slope(INPUT), 154);
     }
 }
-
