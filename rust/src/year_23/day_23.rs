e6695601063fe5bfffcb2d23c14b1146c6dc22f7
Ammar Abou Zor <ammarabouzor@gmail.com>
2023-12-23 17:07:21 +0100

    Rust: Year 23: Day 23: Part 2: Naive approach
    - Brute Forcing worked on the example but not on the real data

--- rust/src/year_23/day_23.rs
+++ rust/src/year_23/day_23.rs
@@ -153,6 +153,69 @@ impl Maze {
 
         max_steps
     }
+
+    fn get_next_moves_no_slope(&self, state: &State) -> Vec<Point> {
+        [
+            Point::new(state.curr.row - 1, state.curr.col),
+            Point::new(state.curr.row + 1, state.curr.col),
+            Point::new(state.curr.row, state.curr.col - 1),
+            Point::new(state.curr.row, state.curr.col + 1),
+        ]
+        .into_iter()
+        .filter(|p| {
+            *p != state.prev && matches!(self.get_content(p), b'.' | b'v' | b'^' | b'<' | b'>')
+        })
+        .collect()
+    }
+
+    fn find_max_steps_no_slope(&self) -> usize {
+        // real input and the test has the first entrance
+        let initial_state = State::new(Point::new(1, 1), Point::new(0, 1), Vec::new());
+        let mut queue = VecDeque::new();
+        // We've already made the first step
+        queue.push_back((1, initial_state.clone()));
+
+        let mut states_map: HashMap<State, usize> = HashMap::new();
+
+        let mut max_steps = 0;
+
+        while let Some((mut steps, state)) = queue.pop_front() {
+            if self.reached_target(&state.curr) {
+                max_steps = max_steps.max(steps);
+                continue;
+            }
+
+            if let Some(saved_steps) = states_map.get_mut(&state) {
+                if steps > *saved_steps {
+                    *saved_steps = steps;
+                } else {
+                    continue;
+                }
+            }
+
+            steps += 1;
+            for p in self.get_next_moves_no_slope(&state) {
+                let mut clone = state.clone();
+                let next_point = match self.get_content(&p) {
+                    b'^' | b'v' | b'<' | b'>' | b'.' => {
+                        if clone.visited_slopes.contains(&p) {
+                            continue;
+                        }
+                        clone.visited_slopes.push(p);
+
+                        p
+                    }
+                    invalid => unreachable!("Invalid byte: '{invalid}'"),
+                };
+
+                clone.prev = clone.curr;
+                clone.curr = next_point;
+                queue.push_back((steps, clone));
+            }
+        }
+
+        max_steps
+    }
 }
 
 fn find_max_steps(input: &'static str) -> usize {
@@ -161,13 +224,23 @@ fn find_max_steps(input: &'static str) -> usize {
     maze.find_max_steps()
 }
 
+fn find_max_no_slope(input: &'static str) -> usize {
+    let maze = Maze::from(input);
+
+    maze.find_max_steps_no_slope()
+}
+
 fn part_1(input: &'static str) {
     let answer = find_max_steps(input);
 
     println!("Part 1 answer is {answer}");
 }
 
-fn part_2(input: &'static str) {}
+fn part_2(input: &'static str) {
+    let answer = find_max_no_slope(input);
+
+    println!("Part 2 answer is {answer}");
+}
 
 pub fn run() {
     //TODO: uncomment the first input and remove the later when the solution is solved
@@ -208,6 +281,7 @@ mod test {
     #[test]
     fn test_solution() {
         assert_eq!(find_max_steps(INPUT), 94);
+        assert_eq!(find_max_no_slope(INPUT), 154);
     }
 }
 
