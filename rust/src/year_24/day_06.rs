6c4a2a39b12409366b0961175f5a1dc724427002
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-12-06 08:10:24 +0100

    Rust: Year 24: Day 6: Part 2

--- rust/src/year_24/day_06.rs
+++ rust/src/year_24/day_06.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 enum Direction {
     Up,
     Right,
@@ -68,8 +68,11 @@ fn calc_visited(input: &str) -> usize {
             Some(ch) => ch,
             None => break,
         };
-        dir = match next_char {
-            '#' => dir.rotate(),
+        match next_char {
+            '#' => {
+                dir = dir.rotate();
+                continue;
+            }
             _ => dir,
         };
         cur_pos = dir.next(cur_pos.0, cur_pos.1);
@@ -78,12 +81,79 @@ fn calc_visited(input: &str) -> usize {
     visited.len()
 }
 
+fn is_circle(grid: Vec<Vec<char>>, mut cur_pos: (usize, usize), mut dir: Direction) -> bool {
+    let mut visited = HashSet::new();
+
+    loop {
+        let next_pos = dir.next(cur_pos.0, cur_pos.1);
+        let next_char = match grid.get(next_pos.0).and_then(|row| row.get(next_pos.1)) {
+            Some(ch) => ch,
+            None => return false,
+        };
+        match next_char {
+            '#' => {
+                dir = dir.rotate();
+                continue;
+            }
+            _ => {}
+        };
+
+        if !visited.insert((cur_pos, dir)) {
+            return true;
+        }
+
+        cur_pos = dir.next(cur_pos.0, cur_pos.1);
+    }
+}
+
+fn calc_circle(input: &'static str) -> usize {
+    let grid = parse(input);
+    let (start_pos, start_dir) = grid
+        .iter()
+        .enumerate()
+        .find_map(|(row, vec)| {
+            vec.iter().enumerate().find_map(|(col, char)| {
+                (char == &'^').then(|| {
+                    let pos = (row, col);
+                    let dir = Direction::from(*char);
+                    (pos, dir)
+                })
+            })
+        })
+        .unwrap();
+
+    let mut answer = 0;
+
+    for row in 0..grid.len() {
+        for col in 0..grid[0].len() {
+            match grid[row][col] {
+                // '.' => {}
+                '#' | '^' => continue,
+                // _ => continue,
+                _ => {}
+            };
+
+            let mut clone = grid.clone();
+            clone[row][col] = '#';
+            if is_circle(clone, start_pos, start_dir) {
+                // println!("({row}, {col})");
+                answer += 1;
+            }
+        }
+    }
+
+    answer
+}
+
 fn part_1(input: &'static str) {
     let visited = calc_visited(input);
     println!("Part 1 answer is {visited}");
 }
 
-fn part_2(input: &'static str) {}
+fn part_2(input: &'static str) {
+    let circle = calc_circle(input);
+    println!("Part 2 answer is {circle}")
+}
 
 pub fn run() {
     let input = crate::utls::read_text_from_file("24", "06").leak();
@@ -110,6 +180,8 @@ mod test {
     fn test_solution() {
         let visited = calc_visited(INPUT);
         assert_eq!(visited, 41);
+
+        let circle = calc_circle(INPUT);
+        assert_eq!(circle, 6)
     }
 }
-
