f914b95964a95594d293acd0f7642ebb9e364687
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-12-08 07:26:00 +0100

    Rust: Year 24: Day 8: Part 2

--- rust/src/year_24/day_08.rs
+++ rust/src/year_24/day_08.rs
@@ -12,6 +12,10 @@ impl Pos {
     fn new(row: i32, col: i32) -> Self {
         Self { row, col }
     }
+
+    fn is_inside(&self, rows_count: i32, cols_count: i32) -> bool {
+        self.row >= 0 && self.col >= 00 && self.row < rows_count && self.col < cols_count
+    }
 }
 
 fn parse(input: &str) -> Vec<Vec<char>> {
@@ -34,7 +38,7 @@ fn get_antennas(grid: &[Vec<char>]) -> HashMap<char, Vec<Pos>> {
     antennas
 }
 
-fn antenna_antinodes(positions: &[Pos]) -> Vec<Pos> {
+fn antenna_antinodes_once(positions: &[Pos]) -> Vec<Pos> {
     let mut res = Vec::with_capacity(positions.len() * 2);
     for cur in positions {
         for other in positions {
@@ -54,33 +58,70 @@ fn antenna_antinodes(positions: &[Pos]) -> Vec<Pos> {
     res
 }
 
-fn unique_antinodes(input: &str) -> usize {
+fn unique_antinodes_once(input: &str) -> usize {
     let grid = parse(input);
     let antennas = get_antennas(&grid);
 
     antennas
         .iter()
-        // .take(1)
         .flat_map(|(_name, poses)| {
-            let antinodes = antenna_antinodes(poses);
+            let antinodes = antenna_antinodes_once(poses);
             antinodes
         })
-        .filter(|antinode| {
-            antinode.row >= 0
-                && antinode.col >= 0
-                && antinode.row < grid.len() as i32
-                && antinode.col < grid[0].len() as i32
-        })
+        .filter(|antinode| antinode.is_inside(grid.len() as i32, grid[0].len() as i32))
         .unique()
         .count()
 }
 
 fn part_1(input: &'static str) {
-    let ans = unique_antinodes(input);
+    let ans = unique_antinodes_once(input);
     println!("Part 1 answer is {ans}")
 }
 
-fn part_2(input: &'static str) {}
+fn antenna_antinodes_all(positions: &[Pos], rows: i32, cols: i32) -> Vec<Pos> {
+    let mut res = Vec::with_capacity(positions.len() * 2);
+    for cur in positions {
+        for other in positions {
+            if cur == other {
+                continue;
+            }
+            let dif_r = other.row - cur.row;
+            let dif_c = other.col - cur.col;
+
+            for factor in 0.. {
+                let row = cur.row - (factor * dif_r);
+                let col = cur.col - (factor * dif_c);
+                let pos = Pos::new(row, col);
+                if pos.is_inside(rows, cols) {
+                    res.push(pos);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    res
+}
+
+fn unique_antinodes_all(input: &str) -> usize {
+    let grid = parse(input);
+    let antennas = get_antennas(&grid);
+
+    antennas
+        .iter()
+        .flat_map(|(_name, poses)| {
+            let antinodes = antenna_antinodes_all(poses, grid.len() as i32, grid[0].len() as i32);
+            antinodes
+        })
+        .unique()
+        .count()
+}
+
+fn part_2(input: &'static str) {
+    let ans = unique_antinodes_all(input);
+    println!("Part 2 answer is {ans}")
+}
 
 pub fn run() {
     let input = crate::utls::read_text_from_file("24", "08").leak();
@@ -107,8 +148,11 @@ mod test {
 
     #[test]
     fn test_solution() {
-        let count = unique_antinodes(INPUT);
+        let count = unique_antinodes_once(INPUT);
         assert_eq!(count, 14);
+
+        let all_count = unique_antinodes_all(INPUT);
+        assert_eq!(all_count, 34);
     }
 }
 
